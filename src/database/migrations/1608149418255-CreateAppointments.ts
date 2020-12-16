091213e97a28c06d473b32ab4c1a5213a709947b
Bruno Lacerda <brunolcpg@gmail.com>
2020-12-16 18:24:39 -0300

    Add uuid function to migration

--- src/database/migrations/1608149418255-CreateAppointments.ts
+++ src/database/migrations/1608149418255-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1608149418255
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
