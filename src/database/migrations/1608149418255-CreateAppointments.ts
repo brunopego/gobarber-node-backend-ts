cae9e3bd9eaf9064952e3686dd963b1acbe0949a
Bruno Lacerda <brunolcpg@gmail.com>
2020-12-16 22:19:00 -0300

    Update appointments migration and model

--- src/database/migrations/1608149418255-CreateAppointments.ts
+++ src/database/migrations/1608149418255-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1608149418255
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
