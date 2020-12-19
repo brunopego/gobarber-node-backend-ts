7c24d363aba19ad1c24e09d103d86b84938b5bb1
Bruno Lacerda <brunolcpg@gmail.com>
2020-12-19 10:44:39 -0300

    Avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -25,11 +26,12 @@ usersRouter.post('/', async (request, response) => {
       id: user.id,
       name: user.name,
       email: user.email,
+      avatar: user.avatar,
       created_at: user.created_at,
       updated_at: user.updated_at,
     };
 
-    return response.json(userWithoutPassword);
+    return response.json({ user: userWithoutPassword });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -40,8 +42,27 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json({ user: userWithoutPassword });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
